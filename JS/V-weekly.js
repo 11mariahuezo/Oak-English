aa6badc99ebd5d651fa3a968364da8576430f20e
11mariahuezo <mariajosehuezoramirez4@gmail.com>
2026-09-03 18:47:24 -0600

    modifique algunas cosas

--- JS/V-weekly.js
+++ JS/V-weekly.js
@@ -3,91 +3,95 @@
             berries: {
                 title: "Weekly vocabulary: berries",
                 items: [
-                    { eng: "Strawberry", esp: "Fresa", def: "A small juicy red fruit that has tiny seeds on its surface." },
-                    { eng: "Blueberry", esp: "Arándano", def: "A small sweet blue fruit that grows on a bush." },
-                    { eng: "Raspberry", esp: "Frambuesa", def: "A small soft red fruit that grows on bushes." },
-                    { eng: "Cherry", esp: "Cereza", def: "A small, round, soft red or black fruit with a stone." }
+                    { eng: "Strawberry", 
+                        esp: "Fresa", 
+                        def: "A small juicy red fruit that has tiny seeds on its surface.", 
+                        img:"https://i.pinimg.com/736x/48/32/7b/48327baffcf427874aeeabe915abbf94.jpg"},
+
+                    { eng: "Blueberry", esp: "Arándano", def: "A small sweet blue fruit that grows on a bush.", img:"https://i.pinimg.com/736x/7a/c1/db/7ac1dbe96a6da5b270a066fcd703194d.jpg"},
+                    { eng: "Raspberry", esp: "Frambuesa", def: "A small soft red fruit that grows on bushes.", img:"https://i.pinimg.com/736x/f1/28/cb/f128cbda92ef3d5764ef1a32ea9929a6.jpg"},
+                    { eng: "Cherry", esp: "Cereza", def: "A small, round, soft red or black fruit with a stone.", img:"https://i.pinimg.com/736x/23/b8/18/23b818173675376ad9b34ee0c1c7afd6.jpg"}
                 ]
             },
             clothes: {
                 title: "Weekly vocabulary: clothes",
                 items: [
-                    { eng: "Shirt", esp: "Camisa", def: "A piece of clothing worn on the upper part of the body." },
-                    { eng: "Pants", esp: "Pantalones", def: "An outer garment covering the body from the waist to the ankles." },
-                    { eng: "Jacket", esp: "Chaqueta", def: "A short coat that covers the upper body against cold weather." },
-                    { eng: "Shoes", esp: "Zapatos", def: "Coverings for feet, usually made of leather or plastic." }
+                    { eng: "Shirt", esp: "Camisa", def: "A piece of clothing worn on the upper part of the body.", img:"https://i.pinimg.com/736x/c7/f7/4a/c7f74ad3dfaf525777451a1a10cb5dac.jpg" },
+                    { eng: "Pants", esp: "Pantalones", def: "An outer garment covering the body from the waist to the ankles.", img:"https://i.pinimg.com/736x/58/ca/69/58ca6936018c018eb9f0fbb9ff16ee47.jpg"},
+                    { eng: "Jacket", esp: "Chaqueta", def: "A short coat that covers the upper body against cold weather.", img:"https://i.pinimg.com/736x/17/bc/6c/17bc6cc5c2819e69b61d97c37c14727a.jpg" },
+                    { eng: "Shoes", esp: "Zapatos", def: "Coverings for feet, usually made of leather or plastic.", img:"https://i.pinimg.com/736x/a4/18/7e/a4187ea0da3c7542bc7b817b95af5592.jpg" }
                 ]
             },
             classroom: {
                 title: "Weekly vocabulary: classroom materials",
                 items: [
-                    { eng: "Pencil Case", esp: "Cartuchera", def: "A small container used to store pencils, pens, and erasers." },
-                    { eng: "Pencil", esp: "Lápiz", def: "An instrument for writing or drawing, consisting of a thin stick of graphite." },
-                    { eng: "Notebook", esp: "Cuaderno", def: "A book with blank pages for recording notes or homework." },
-                    { eng: "Backpack", esp: "Mochila", def: "A bag with straps that go over your shoulders to carry school supplies." }
+                    { eng: "Pencil Case", esp: "Cartuchera", def: "A small container used to store pencils, pens, and erasers.", img:"https://i.pinimg.com/736x/81/a5/c7/81a5c7a702a2c7d8a9689da5a69328e3.jpg" },
+                    { eng: "Pencil", esp: "Lápiz", def: "An instrument for writing or drawing, consisting of a thin stick of graphite.", img:"https://i.pinimg.com/736x/44/b6/7d/44b67d466dc59d923fae9b10ef3085a6.jpg" },
+                    { eng: "Notebook", esp: "Cuaderno", def: "A book with blank pages for recording notes or homework.", img:"https://i.pinimg.com/736x/38/9e/6e/389e6ee08292abf368ce8d13785f6b19.jpg"},
+                    { eng: "Backpack", esp: "Mochila", def: "A bag with straps that go over your shoulders to carry school supplies.", img:"https://i.pinimg.com/736x/90/1d/e8/901de856b57514d1004c124fe79adacc.jpg"}
                 ]
             },
             fruits: {
                 title: "Weekly vocabulary: fruits",
                 items: [
-                    { eng: "Apple", esp: "Manzana", def: "A round fruit with red, green, or yellow skin and crisp white flesh." },
-                    { eng: "Banana", esp: "Guineo / Plátano", def: "A long curved fruit with a thick yellow skin and soft sweet flesh." },
-                    { eng: "Orange", esp: "Naranja", def: "A round juicy citrus fruit with a bright reddish-yellow rind." },
-                    { eng: "Watermelon", esp: "Sandía", def: "A large melon with a hard green rind and sweet juicy red pulp." }
+                    { eng: "Apple", esp: "Manzana", def: "A round fruit with red, green, or yellow skin and crisp white flesh.", img:"https://i.pinimg.com/736x/89/31/5b/89315b0a4e87a9493b3f0972e60f6f87.jpg" },
+                    { eng: "Banana", esp: "Guineo / Plátano", def: "A long curved fruit with a thick yellow skin and soft sweet flesh.", img:"https://i.pinimg.com/1200x/42/d7/2a/42d72a0bf2973fcf5d3bc5f5556e882a.jpg" },
+                    { eng: "Orange", esp: "Naranja", def: "A round juicy citrus fruit with a bright reddish-yellow rind.",  img:"https://i.pinimg.com/1200x/4f/78/2c/4f782cd857d7db20ec02e6bcf0df9650.jpg" },
+                    { eng: "Watermelon", esp: "Sandía", def: "A large melon with a hard green rind and sweet juicy red pulp.", img:"https://i.pinimg.com/1200x/57/2e/44/572e446e7ecd1261d1973b7e11f1d622.jpg"}
                 ]
             },
             colors: {
                 title: "Weekly vocabulary: colors",
                 items: [
-                    { eng: "Red", esp: "Rojo", def: "The color of blood, rubies, or ripe strawberries." },
-                    { eng: "Blue", esp: "Azul", def: "The color of the clear sky or the deep ocean." },
-                    { eng: "Yellow", esp: "Amarillo", def: "The color of lemons, ripe bananas, or the bright sun." },
-                    { eng: "Green", esp: "Verde", def: "The color of growing grass, leaves, and emeralds." }
+                    { eng: "Red", esp: "Rojo", def: "The color of blood, rubies, or ripe strawberries.", img:"https://i.pinimg.com/1200x/57/2e/44/572e446e7ecd1261d1973b7e11f1d622.jpg" },
+                    { eng: "Blue", esp: "Azul", def: "The color of the clear sky or the deep ocean.", img:"https://i.pinimg.com/736x/d1/32/f0/d132f0fa2cb01b66bea35307ca78b490.jpg" },
+                    { eng: "Yellow", esp: "Amarillo", def: "The color of lemons, ripe bananas, or the bright sun.", img:"https://i.pinimg.com/736x/ed/87/91/ed87919ef58e48e7de217e39fff81804.jpg" },
+                    { eng: "Green", esp: "Verde", def: "The color of growing grass, leaves, and emeralds.",img:"https://i.pinimg.com/736x/c5/bf/87/c5bf877f5164ce5caedb0e588d509d0e.jpg" }
                 ]
             },
             animals: {
                 title: "Weekly vocabulary: animals",
                 items: [
-                    { eng: "Dog", esp: "Perro", def: "A common domesticated animal known for being a loyal pet." },
-                    { eng: "Cat", esp: "Gato", def: "A small domesticated carnivorous mammal with soft fur." },
-                    { eng: "Lion", esp: "León", def: "A large powerful wild cat of Africa, known as the king of the jungle." },
-                    { eng: "Elephant", esp: "Elefante", def: "A very large mammal with a long trunk and large tusks." }
+                    { eng: "Dog", esp: "Perro", def: "A common domesticated animal known for being a loyal pet.", img:"https://i.pinimg.com/736x/9e/3d/b8/9e3db83aef19e412c1b14aec96d4e1c4.jpg" },
+                    { eng: "Cat", esp: "Gato", def: "A small domesticated carnivorous mammal with soft fur." , img:"https://i.pinimg.com/1200x/77/8f/9b/778f9b698a1a26b11b7833997f2cdd82.jpg"},
+                    { eng: "Lion", esp: "León", def: "A large powerful wild cat of Africa, known as the king of the jungle.", img:"https://i.pinimg.com/1200x/58/2a/82/582a82e0ae4b877e1d0a4832b2469666.jpg" },
+                    { eng: "Elephant", esp: "Elefante", def: "A very large mammal with a long trunk and large tusks.", img:"https://i.pinimg.com/1200x/01/8c/62/018c62f920ea75184417d132aeec5a96.jpg"}
                 ]
             },
             family: {
                 title: "Weekly vocabulary: family members",
                 items: [
-                    { eng: "Mother", esp: "Madre", def: "A female parent who cares for and raises children." },
-                    { eng: "Father", esp: "Padre", def: "A male parent of a child or family leader." },
-                    { eng: "Brother", esp: "Hermano", def: "A boy or man in relation to other children of his parents." },
-                    { eng: "Sister", esp: "Hermana", def: "A girl or woman in relation to other children of her parents." }
+                    { eng: "Mother", esp: "Madre", def: "A female parent who cares for and raises children.", img:"https://i.pinimg.com/736x/42/27/41/4227418a9ca299af552d9a3b945577fe.jpg" },
+                    { eng: "Father", esp: "Padre", def: "A male parent of a child or family leader." , img:"https://i.pinimg.com/1200x/e7/e8/a0/e7e8a0012db004cdfa2832386107d557.jpg"},
+                    { eng: "Brother", esp: "Hermano", def: "A boy or man in relation to other children of his parents.", img:"https://i.pinimg.com/736x/fe/18/7b/fe187b364324d8d755f618e19a7f8c37.jpg" },
+                    { eng: "Sister", esp: "Hermana", def: "A girl or woman in relation to other children of her parents.", img:"https://i.pinimg.com/736x/fb/b1/2d/fbb12d82aef2031c534c39685e906681.jpg" }
                 ]
             },
             weather: {
                 title: "Weekly vocabulary: weather",
                 items: [
-                    { eng: "Sunny", esp: "Soleado", def: "Bright with sunlight and minimal or no clouds." },
-                    { eng: "Rainy", esp: "Lluvioso", def: "Having a great deal of rainfall over a period." },
-                    { eng: "Windy", esp: "Ventoso", def: "Characterized by strong winds or moving air." },
-                    { eng: "Cloudy", esp: "Nublado", def: "Overcast with clouds, blocking direct sunlight." }
+                    { eng: "Sunny", esp: "Soleado", def: "Bright with sunlight and minimal or no clouds.", img:"https://i.pinimg.com/1200x/f8/fe/74/f8fe74e49374c31ec8dd2dd9471b154f.jpg" },
+                    { eng: "Rainy", esp: "Lluvioso", def: "Having a great deal of rainfall over a period.", img:"https://i.pinimg.com/1200x/7b/11/6b/7b116bc5e657189c3cf2f0ed18e22c48.jpg" },
+                    { eng: "Windy", esp: "Ventoso", def: "Characterized by strong winds or moving air.", img:"" },
+                    { eng: "Cloudy", esp: "Nublado", def: "Overcast with clouds, blocking direct sunlight.", img:""}
                 ]
             },
             sports: {
                 title: "Weekly vocabulary: sports",
                 items: [
-                    { eng: "Soccer", esp: "Fútbol", def: "A game played by two teams of eleven players with a round ball." },
-                    { eng: "Basketball", esp: "Baloncesto", def: "A game played on a court where players throw a ball into a high hoop." },
-                    { eng: "Tennis", esp: "Tenis", def: "A sport in which two or four players use rackets to hit a ball over a net." },
-                    { eng: "Swimming", esp: "Natación", def: "The sport or activity of moving through water using your limbs." }
+                    { eng: "Soccer", esp: "Fútbol", def: "A game played by two teams of eleven players with a round ball.", img:""},
+                    { eng: "Basketball", esp: "Baloncesto", def: "A game played on a court where players throw a ball into a high hoop.", img:"" },
+                    { eng: "Tennis", esp: "Tenis", def: "A sport in which two or four players use rackets to hit a ball over a net.", img:"" },
+                    { eng: "Swimming", esp: "Natación", def: "The sport or activity of moving through water using your limbs.", img:"" }
                 ]
             },
             house: {
                 title: "Weekly vocabulary: house rooms",
                 items: [
-                    { eng: "Kitchen", esp: "Cocina", def: "A room or area where food is prepared and cooked." },
-                    { eng: "Bedroom", esp: "Dormitorio", def: "A room used primarily for sleeping and resting." },
-                    { eng: "Bathroom", esp: "Baño", def: "A room containing a toilet and tub or shower for personal hygiene." },
-                    { eng: "Living Room", esp: "Sala de Estar", def: "A room in a house for general and everyday social use." }
+                    { eng: "Kitchen", esp: "Cocina", def: "A room or area where food is prepared and cooked.", img:"" },
+                    { eng: "Bedroom", esp: "Dormitorio", def: "A room used primarily for sleeping and resting.", img:"" },
+                    { eng: "Bathroom", esp: "Baño", def: "A room containing a toilet and tub or shower for personal hygiene.", img:"" },
+                    { eng: "Living Room", esp: "Sala de Estar", def: "A room in a house for general and everyday social use.", img:"" }
                 ]
             }
         };
@@ -135,15 +139,32 @@
                 grid.innerHTML += `
                     <div class="col">
                         <div class="vocabulary-card" onclick="document.getElementById('footer-section').scrollIntoView({behavior: 'smooth'})">
-                            <div class="image-placeholder">
-                                [Image: ${item.eng.replace("The ", "")}]
-                            </div>
-                            <div class="card-content-area">
-                                <h3 class="card-title">${item.eng}</h3>
-                                <div class="card-subtitle">${item.esp}</div>
-                                <div class="card-definition-label">Definition:</div>
-                                <p class="card-definition-text">${item.def}</p>
-                            </div>
+                    
+                                <div class="image-placeholder">
+                                    <img src="${item.img}" 
+                                    alt="${item.eng}">
+                                </div>
+
+                                <div class="card-content-area">
+
+                                    <h3 class="card-title">
+                                        ${item.eng}
+                                    </h3>
+
+                                    <div class="card-subtitle">
+                                        ${item.esp}
+                                    </div>
+
+                                    <div class="card-definition-label">
+                                        Definition:
+                                    </div>
+
+                                    <p class="card-definition-text">
+                                        ${item.def}
+                                    </p>
+
+                                </div>
+
                         </div>
                     </div>
                 `;
